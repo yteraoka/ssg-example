e801a58f58abf3f70f4fd6ed5b393e1948e8d698
Yoshinori Teraoka <teraoka@3-shake.com>
2022-08-09 12:04:10 +0900

    PReview 時に backend server を指定可能にする

--- mysite/src/pages/Index.vue
+++ mysite/src/pages/Index.vue
@@ -14,6 +14,10 @@
       お客様が脂肪分解プロセスに注意を払うことは非常に重要です。彼はその時点で労働を受け取るに違いありません、そして彼はそれを受け取る義務があります、私は説明します!ドロレス、荒くれ者のサービスは開かない
     </p>
 
+    <p>
+      API Server: document.domain
+    </p>
+
     <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
